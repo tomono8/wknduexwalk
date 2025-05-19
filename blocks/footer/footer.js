aeac9c54d97bcf9d4ad066f6884a09007559eea0
tomono8 <tomono@adobe.com>
2025-05-19 23:45:58 +0900

    wknd publish

---
+++ blocks/footer/footer.js
@@ -0,0 +1,30 @@
+import { decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
+import { getSiteRoot } from '../../scripts/scripts.js';
+
+/**
+ * loads and decorates the footer
+ * @param {Element} block The header block element
+ */
+
+export default async function decorate(block) {
+  const footerMeta = getMetadata('footer');
+  const navPath = footerMeta ? new URL(footerMeta, window.location).pathname : (window.wknd.demoConfig.demoBase || '/footer');
+
+  const resp = await fetch(`${getSiteRoot()}${navPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+  if (resp.ok) {
+    block.textContent = '';
+
+    const html = await resp.text();
+    const footer = document.createElement('div');
+    footer.innerHTML = html;
+    await decorateIcons(footer);
+
+    const classes = ['brand', 'nav', 'follow', 'disc'];
+    let f = footer.firstElementChild;
+    while (f && classes.length) {
+      f.classList.add(classes.shift());
+      f = f.nextElementSibling;
+    }
+    block.append(footer);
+  }
+}
